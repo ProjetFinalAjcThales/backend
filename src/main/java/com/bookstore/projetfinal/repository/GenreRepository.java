8d68656949b1092fb42df50938a3b9267648d235
Jeanne Siché <67587682+JeanneSic@users.noreply.github.com>
2022-10-24 17:29:39 +0200

    Revert "try #1 merge crud_livre into dev"

--- src/main/java/com/bookstore/projetfinal/repository/GenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.projetfinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookstore.projetfinal.model.Genre;
-
-public interface GenreRepository extends JpaRepository<Genre, Integer>{
-
-}
